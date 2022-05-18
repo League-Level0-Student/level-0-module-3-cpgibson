0536ef16798f6e98e8637abea90f27e7dee7fbd4
root <root@ff3991d8099f>
2022-05-18 01:23:05 +0000

    commit

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,6 +2,9 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
@@ -14,11 +17,33 @@ public class RobotColorChooser {
 		//5. Use an if/else statement to set the pen color that the user requested
         //6. If the user doesn't enter anything, choose a random color
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-
-		
-
-
-
+		Robot rob = new Robot("mini");
+		rob.penDown();
+		rob.setPenWidth(10);
+		String request = JOptionPane.showInputDialog("What color to you want the robot to draw with?");
+		if (request.equalsIgnoreCase("red")){
+			rob.setPenColor(Color.RED);
+		}
+		if (request.equalsIgnoreCase("orange")) {
+			rob.setPenColor(Color.ORANGE);
+		}
+		if (request.equalsIgnoreCase("yellow")) {
+			rob.setPenColor(Color.YELLOW);
+		}
+		if (request.equalsIgnoreCase("green")) {
+			rob.setPenColor(Color.GREEN);
+		}
+		if (request.equalsIgnoreCase("blue")) {
+			rob.setPenColor(Color.BLUE);
+		}
+		if (request.equalsIgnoreCase("purple")) {
+			rob.setPenColor(Color.MAGENTA);
+		}
+	 
+			rob.move(40);
+			rob.setAngle(60);
+			rob.move(40);
+			rob.setAngle(120);
+			rob.move(40);
 	}
 }
